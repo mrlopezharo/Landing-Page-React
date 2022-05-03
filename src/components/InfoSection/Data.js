6b0ddb1ffd1f289c4d31460743c7b3c180882c36
Ricardo Lopez <81579356+mrlopezharo@users.noreply.github.com>
2022-05-03 14:48:01 -0600

    Add files via upload

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Inlimited transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false /*change image site*/,
+  img: require("../../images/svg-1.svg").default,
+  alt: "Bitcoin",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Login to your account at any time",
+  description:
+    "We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.",
+  buttonLabel: "Learn More",
+  imgStart: true /*change image site*/,
+  img: require("../../images/img2.svg").default,
+  alt: "Finance",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Get everthing set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now",
+  imgStart: false /*change image site*/,
+  img: require("../../images/img3.svg").default,
+  alt: "Paper",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import Services from "../components/Services";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import {
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree,
+} from "../components/InfoSection/Data";
+
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Services />
+      <InfoSection {...homeObjThree} />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
